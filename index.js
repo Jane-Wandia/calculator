f678b93a63e98e979c06cf44c1a095766262016f
Jane-Wandia <janekigenyi2018@gmail.com>
2022-04-15 01:05:50 +0300

    added buttons and divs

---
+++ index.js
@@ -0,0 +1,33 @@
+/*class Calcurator{
+constructor(previosOperandTextElement, currentOperandTextElement) {
+    this.previosOperandTextElement = previosOperandTextElement
+    this.previosOperandTextElement = previosOperandTextElement
+}
+clear(){
+
+}
+
+delete(){
+
+}
+
+appendNumber(number){
+
+}
+
+chooseOperation(operation){
+
+}
+}
+
+
+
+
+const numberButtons = document.querySelectorAll("[data-number]")
+const operatiosButtons = document.querySelectorAll("[data-operation")
+const equalsButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelectorAll("[data-delete]")
+const allCrearButton = document.querySelectorAll("[data-all-clear]")
+const previosOperandTextElementButton = document.querySelector("[data-previous-operand]")
+const currentOperandTextElementButton = document.querySelector("[data-current-operand]")
+
